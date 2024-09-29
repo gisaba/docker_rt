28d67ee450240e4c9e83470a98a4445d5e781c4c
gisaba <gisaba1980@gmail.com>
2024-09-29 12:20:16 +0200

    fix install docker

--- install_realtime_kernel.sh
+++ install_realtime_kernel.sh
@@ -151,26 +151,29 @@ disable_unnecessary_services() {
 
 # Funzione per installare Docker
 install_docker() {
-    # Installare le dipendenze di Docker
-    apt update
-    apt install -y apt-transport-https ca-certificates curl software-properties-common
+    # Add Docker's official GPG key:
+    apt-get update
+    apt-get install ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
 
-    # Aggiungi la chiave GPG di Docker e il repository ufficiale
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo tee /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=armhf signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
 
     # Aggiorna e installa Docker
     apt update
-    apt install -y docker-ce docker-ce-cli containerd.io
+    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     # Abilita Docker al boot
     systemctl enable docker
     
     # Post installation script
     usermod -aG docker $SUDO_USER
-
-    
-
 }
 
 tune_system_for_realtime() {
