fb19dac32acb78061997abb4c325c83bbfb464b3
Antonio Picone <info@antoniopicone.it>
2024-09-30 11:52:45 +0200

    Added -y option to apt-get install in docker_install function

--- install_realtime_kernel.sh
+++ install_realtime_kernel.sh
@@ -153,7 +153,7 @@ disable_unnecessary_services() {
 install_docker() {
     # Add Docker's official GPG key:
     apt-get update
-    apt-get install ca-certificates curl
+    apt-get install -y ca-certificates curl
     install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
     chmod a+r /etc/apt/keyrings/docker.asc
@@ -167,7 +167,7 @@ install_docker() {
 
     # Aggiorna e installa Docker
     apt update
-    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     # Abilita Docker al boot
     systemctl enable docker
