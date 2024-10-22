5e8f7014280c726f3aa8bee0ff8b3b57fe03c1d5
gisaba <gisaba1980@gmail.com>
2024-10-22 23:49:24 +0200

    review

--- matlab_post_analisys/analisi_dati.m
+++ matlab_post_analisys/analisi_dati.m
@@ -8,11 +8,11 @@ t_deadline = 1;
 %test = 'lpo';
 test = 'fft';
 
-%profile = 'real-time';
-profile = 'balanced';
+profile = 'real-time';
+%profile = 'balanced';
 
 %linguaggio = 'python';
-%linguaggio = 'csharp
+%linguaggio = 'csharp'
 linguaggio = 'c';
 
 %so = 'linux';
