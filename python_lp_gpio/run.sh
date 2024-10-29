8135ea42b2ca7dc5820fdcbadbf1f8feecf3eda1
gisaba <gisaba1980@gmail.com>
2024-10-29 21:25:56 +0100

    localhost

--- python_lp_gpio/run.sh
+++ python_lp_gpio/run.sh
@@ -1,3 +1,3 @@
 docker build -t gpiotest .
 
-docker run --cpuset-cpus="2-3" --ulimit rtprio=99 --cap-add=sys_nice --security-opt seccomp=unconfined --privileged --name gpio_test_py -it --rm --device /dev/gpiochip4:/dev/gpiochip4 gpiotest
+docker run --network=host --cpuset-cpus="2-3" --ulimit rtprio=99 --cap-add=sys_nice --security-opt seccomp=unconfined --privileged --name gpio_test_py -it --rm --device /dev/gpiochip4:/dev/gpiochip4 gpiotest
