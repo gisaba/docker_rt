e18849c11312b50cea4c185bbc959efdcdf7be9b
gisaba <gisaba1980@gmail.com>
2024-10-29 21:49:03 +0100

    led_line.set_value(0) # Turn off the LED

--- python_lp_gpio/script-lpo-gpio.py
+++ python_lp_gpio/script-lpo-gpio.py
@@ -124,10 +124,9 @@ if __name__ == "__main__":
     for i in range(1,100000):
         i+=1
         
-        publish_mqtt(1)
         led_line.set_value(1) # Turn on the LED
+        publish_mqtt(10)
         tempo_esecuzione_ms = verifica_tempo_esecuzione(funzione_da_testare, tempo_massimo_ms)
-        led_line.set_value(0) # Turn off the LED
         t_idle_ms = (tempo_massimo_ms-tempo_esecuzione_ms)
         #print(f"{i},{tempo_esecuzione_ms},{tempo_massimo_ms}")
         t_idle = t_idle_ms/1000
@@ -138,10 +137,11 @@ if __name__ == "__main__":
         else:
         """
         publish_mqtt(0) 
+        led_line.set_value(0) # Turn off the LED
         sleep(abs(t_idle))
         
         # Force a garbage collection
-        gc.collect()
+        # gc.collect()
     
     # Release the GPIO line and clean up resources on program exit
 
