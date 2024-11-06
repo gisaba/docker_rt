e13bb252f46c9d42a59ae2d51e9466b65adbbb72
gisaba <gisaba1980@gmail.com>
2024-11-06 22:11:52 +0100

    RPIO

--- python_PWM/test.py
+++ python_PWM/test.py
@@ -1,15 +1,12 @@
-from RPi.GPIO import PWM
+from RPIO import PWM
 
-# Setup PWM and DMA channel 0
-PWM.setup()
-PWM.init_channel(0)
+servo = PWM.Servo()
 
-# Add some pulses to the subcycle
-PWM.add_channel_pulse(0, 17, 0, 50)
-PWM.add_channel_pulse(0, 17, 100, 50)
+# Set servo on GPIO17 to 1200µs (1.2ms)
+servo.set_servo(17, 1200)
 
-# Stop PWM for specific GPIO on channel 0
-PWM.clear_channel_gpio(0, 17)
+# Set servo on GPIO17 to 2000µs (2.0ms)
+servo.set_servo(17, 2000)
 
-# Shutdown all PWM and DMA activity
-PWM.cleanup()
+# Clear servo on GPIO17
+servo.stop_servo(17)
