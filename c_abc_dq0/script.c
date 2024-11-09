a2e3b27d11ca4f0a311726ac6a1d56b89c8632ae
gisaba <gisaba1980@gmail.com>
2024-11-09 15:02:12 +0100

    1 milione

--- c_abc_dq0/script.c
+++ c_abc_dq0/script.c
@@ -86,7 +86,7 @@ int main() {
     float control_output = 0.0f;  // Uscita del controllo PID
 
     // Eseguiamo il controllo per un certo numero di iterazioni
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 1000000; i++) {
 
         set_realtime_priority();
 
