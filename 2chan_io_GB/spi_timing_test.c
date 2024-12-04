c6da305856a1d3acebd4fcf8ae77792ec7b50dfb
gisaba <gisaba1980@gmail.com>
2024-12-04 14:43:31 +0100

    4Mhz

--- 2chan_io_GB/spi_timing_test.c
+++ 2chan_io_GB/spi_timing_test.c
@@ -21,7 +21,7 @@
 // Configurazione CPU e SPI
 #define SPI_CHANNEL_IN    0       // CE0 (GPIO8)  - Canale di lettura
 #define SPI_CHANNEL_OUT   1       // CE1 (GPIO7)  - Canale di scrittura
-#define SPI_SPEED       500000   // 2MHz compatibile con Arduino
+#define SPI_SPEED       4000000   // 2MHz compatibile con Arduino
 #define BITS_PER_TRANSFER 8
 #define BUFFER_SIZE_BITS 32
 #define BUFFER_SIZE_BYTES (BUFFER_SIZE_BITS / 8)
