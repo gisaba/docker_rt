2f583c19bc52a8eac997febd9163969a8aa882c4
gisaba <gisaba1980@gmail.com>
2024-12-04 15:57:41 +0100

    cs

--- 2chan_io_GB/spi_timing_test.c
+++ 2chan_io_GB/spi_timing_test.c
@@ -248,7 +248,7 @@ static void *read_thread(void *arg) {
         #ifdef USE_REAL_SPI
 
             // Turn the LED on
-            digitalWrite(LED_PIN, HIGH);
+            digitalWrite(LED_PIN, LOW);
             
             // Lettura reale SPI (commentata per test)
             if (wiringPiSPIDataRW(SPI_CHANNEL_IN, spi_buffers.read_buffer.bytes, BUFFER_SIZE_BYTES) < 0) {
@@ -278,7 +278,7 @@ static void *read_thread(void *arg) {
         #endif
 
         // Turn the LED off
-        digitalWrite(LED_PIN, LOW);
+        digitalWrite(LED_PIN, HIGH);
 
         //pthread_mutex_lock(&spi_buffers.write_mutex);
         spi_buffers.new_data_available = 1;
